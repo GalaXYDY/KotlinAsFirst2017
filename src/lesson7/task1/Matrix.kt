995c61f87d91ba26d1b72c36d90bccd7750a5731
GalaXYDY <dannyglxy@gmail.com>
2017-12-14 01:37:35 +0300

    Got it ~~~~~~

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,17 +38,17 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
-    if (height * width <= 0) throw IllegalArgumentException()
-    return MatrixImpl<E>(height, width, e)
-}
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+class MatrixImpl<E> : Matrix<E> {
+    override val height: Int = TODO()
+
+    override val width: Int = TODO()
 
     override fun get(row: Int, column: Int): E  = TODO()
 
