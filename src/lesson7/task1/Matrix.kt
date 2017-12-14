4b5cfbd76d30e9cbaeea87c1b3f31faf19bb1ece
GalaXYDY <dannyglxy@gmail.com>
2017-12-14 16:52:22 +0300

    ~~~

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,31 +38,62 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height * width <= 0) throw IllegalArgumentException()
+    return MatrixImpl(height, width, e)
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E> (override val height: Int, override val width: Int, e: E): Matrix<E> {
 
-    override val width: Int = TODO()
+    private val matrixlist = mutableListOf<E>()
+    val multiply = height * width
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    init {
+        for(i in 0 until multiply)
+            matrixlist.add(e)
+    }
 
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(row: Int, column: Int): E  = matrixlist[height * column + row]
+
+    override fun get(cell: Cell): E  = matrixlist[height * cell.column + cell.row]
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        matrixlist[height * column + row] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        matrixlist[height * cell.column + cell.row] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+            other is MatrixImpl<*> &&
+                    height == other.height &&
+                    width == other.width
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width) {
+                sb.append(this[row, column])
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return sb.toString()
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + matrixlist.hashCode()
+        result = 31 * result + multiply
+        return result
+    }
 }
